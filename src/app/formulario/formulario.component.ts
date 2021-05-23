a05c93c652439c08ac29a2c085b4a73dc225425e
daniel M <daniel.marin.depablo@gmail.com>
2021-05-23 18:21:28 +0200

    completado component formulario y links routing

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../interface/post.interface';
 import { ServicesService } from '../service/services.service';
 
 @Component({
@@ -8,9 +10,29 @@ import { ServicesService } from '../service/services.service';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor(private serviceServices: ServicesService) { }
+  newPost: Post[];
+  formulario: FormGroup;
+
+
+  constructor(private serviceServices: ServicesService) {
+    this.formulario = new FormGroup({
+      titulo: new FormControl('', [
+        Validators.required, Validators.minLength(3)
+      ]),
+      texto: new FormControl('', []),
+      autor: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      imagen: new FormControl('', []),
+      categoria: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.newPost = this.formulario.value;
+    console.log(this.newPost);
+
   }
 
 }
