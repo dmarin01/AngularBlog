f0c55b5e43e286e7444e37069dd724ebfb60cc9a
daniel M <daniel.marin.depablo@gmail.com>
2021-05-24 17:04:33 +0200

    espacios

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -30,13 +30,10 @@ export class FormularioComponent implements OnInit {
   }
 
   onSubmit() {
+    this.formulario.value.fecha = new Date();
+    console.log(this.formulario.value);
 
     this.serviceServices.agregarPost(this.formulario.value);
-
     return this.formulario.reset();
-
-
-
   }
-
 }
